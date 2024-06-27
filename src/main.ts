fa91a8667bdeba3d4dd9aa12840645dadca5bac5
volodya-k <volodymyr.korol@izzi.software>
2024-06-27 10:09:38 +0300

    123

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8045);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  app?.getUrl().then(console.log);
 }
 bootstrap();
